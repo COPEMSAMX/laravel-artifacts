05f5af8e56b5b7f49792d11bd777f37339115931
Gregorio Hernández Caso <gregoriohc@gmail.com>
2018-02-06 21:24:37 +0100

    Add base controller

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gregoriohc\Artifacts\Http\Controllers;
+
+use Gregoriohc\Byname\HasByname;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, HasByname;
+
+    protected static function bynameSuffix() {
+        return 'Controller';
+    }
+}
