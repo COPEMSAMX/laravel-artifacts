8c7f60e9ccbb24eb97ee6972346980f38aadc949
Gregorio Hernández Caso <gregoriohc@gmail.com>
2018-02-25 12:35:39 +0100

    Add Constants trait

---
+++ src/Support/Concerns/Constants.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Gregoriohc\Artifacts\Support\Concerns;
+
+use ReflectionClass;
+
+/**
+ * Constants trait for getting classes constants
+ *
+ * Inspired in Sebastiaan Luca's constants trait (https://github.com/sebastiaanluca/laravel-helpers/blob/master/src/Classes/Constants.php)
+ */
+trait Constants
+{
+    /**
+     * Returns class constants, optionally filtered by given prefix
+     *
+     * @param string|null $prefix
+     * @return array
+     * @throws \ReflectionException
+     */
+    public static function constants($prefix = null)
+    {
+        $constants = (new ReflectionClass(__CLASS__))->getConstants();
+
+        if (!is_null($prefix)) {
+            $constants = array_filter($constants, function ($key) use ($prefix) {
+                return stripos($key, $prefix) === 0;
+            }, ARRAY_FILTER_USE_KEY);
+        }
+
+        return $constants;
+    }
+
+    /**
+     * Returns class constants values, optionally filtered by given prefix
+     *
+     * @param string|null $prefix
+     * @return array
+     * @throws \ReflectionException
+     */
+    public static function constantsValues($prefix = null)
+    {
+        return array_values(static::constants($prefix));
+    }
+}
